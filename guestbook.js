e18acf6b221342f86771d2622d2e9777070e2bab
SuperZab-lab <93475630+SuperZab-lab@users.noreply.github.com>
2022-06-09 11:23:46 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,8 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '
-https://0805b12a.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://0805b12a.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
